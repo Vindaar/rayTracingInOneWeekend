a1b9028f1b262dec63d6561f914f2c759ac89a35
Vindaar <basti90@gmail.com>
2021-02-07 13:57:21 +0100

    move hitrecord code to hittables
    Needed for later due to circular dependencies

--- hittables.nim
+++ hittables.nim
@@ -16,6 +16,36 @@ proc `=destroy`*(h: var HittablesList) =
   deallocShared(h.data)
   h.data = nil
   h.len = 0
+  HitRecord* = object
+    p*: Point
+    normal*: Vec3
+    t*: float
+    frontFace*: bool
+    mat*: Material
+
+  Sphere* = object
+    center*: Point
+    radius*: float
+    mat*: Material
+
+
+  MaterialKind* = enum
+    mkLambertian, mkMetal, mkDielectric
+  Material* = object
+    case kind*: MaterialKind
+    of mkLambertian: mLambertian: Lambertian
+    of mkMetal: mMetal: Metal
+    of mkDielectric: mDielectric: Dielectric
+
+  Lambertian* = object
+    albedo*: Color
+
+  Metal* = object
+    albedo*: Color
+    fuzz*: float
+
+  Dielectric* = object
+    ir*: float # refractive index (could use `eta`)
 
 proc initHittables*(size: int = 8): HittablesList =
   ## allocates memory for `size`, but remains empty
@@ -86,3 +116,110 @@ proc hit*(h: var HittablesList, r: Ray, t_min, t_max: float, rec: var HitRecord)
       result = true
       closestSoFar = tmpRec.t
       rec = tmpRec
+
+proc hit*(s: Sphere, r: Ray, t_min, t_max: float, rec: var HitRecord): bool =
+  let oc = r.orig - s.center
+  let a = r.dir.length_squared()
+  let half_b = oc.Vec3.dot(r.dir)
+  let c = oc.length_squared() - s.radius * s.radius
+
+  let discriminant = half_b * half_b - a * c
+  if discriminant < 0:
+    return false
+  let sqrtd = sqrt discriminant
+
+  # find nearest root that lies in acceptable range
+  var root = (-half_b - sqrtd) / a
+  if root < t_min or t_max < root:
+    root = (-half_b + sqrtd) / a
+    if root < t_min or t_max < root:
+      return false
+
+  rec.t = root
+  rec.p = r.at(rec.t)
+  let outward_normal = (rec.p - s.center) / s.radius
+  rec.setFaceNormal(r, outward_normal.Vec3)
+  rec.mat = s.mat
+
+  result = true
+
+proc hit*(h: Hittable, r: Ray, t_min, t_max: float, rec: var HitRecord): bool {.gcsafe.} =
+  case h.kind
+  of htSphere: result = h.hSphere.hit(r, t_min, t_max, rec)
+  of htDisk: result = h.hDisk.hit(r, t_min, t_max, rec)
+  of htBvhNode: result = h.hBvhNode.hit(r, t_min, t_max, rec)
+  of htXyRect: result = h.hXyRect.hit(r, t_min, t_max, rec)
+  of htXzRect: result = h.hXzRect.hit(r, t_min, t_max, rec)
+  of htYzRect: result = h.hYzRect.hit(r, t_min, t_max, rec)
+  of htBox: result = h.hBox.hit(r, t_min, t_max, rec)
+
+proc initLambertian*(a: Color): Lambertian =
+  result = Lambertian(albedo: a)
+
+proc initMetal*(a: Color, f: float): Metal =
+  result = Metal(albedo: a, fuzz: f)
+
+proc initDielectric*(ir: float): Dielectric =
+  result = Dielectric(ir: ir)
+
+proc initMaterial*[T](m: T): Material =
+  when T is Lambertian:
+    result = Material(kind: mkLambertian, mLambertian: m)
+  elif T is Metal:
+    result = Material(kind: mkMetal, mMetal: m)
+  else:
+    result = Material(kind: mkDielectric, mDielectric: m)
+
+proc initSphere*(center: Point, radius: float, mat: Material): Sphere =
+  result = Sphere(center: center, radius: radius, mat: mat)
+proc scatter*(l: Lambertian, r_in: Ray, rec: HitRecord,
+              attenuation: var Color, scattered: var Ray): bool =
+  var scatter_direction = rec.normal + randomUnitVector()
+
+  # catch degenerate scatter direction
+  if scatter_direction.nearZero():
+    scatter_direction = rec.normal
+
+  scattered = initRay(rec.p, scatter_direction)
+  attenuation = l.albedo
+  result = true
+
+proc scatter*(m: Metal, r_in: Ray, rec: HitRecord,
+              attenuation: var Color, scattered: var Ray): bool =
+  var reflected = unitVector(r_in.dir).reflect(rec.normal)
+  scattered = initRay(rec.p, reflected + m.fuzz * randomInUnitSphere())
+  attenuation = m.albedo
+  result = scattered.dir.dot(rec.normal) > 0
+
+proc reflectance(cosine, refIdx: float): float =
+  ## use Schlick's approximation for reflectance
+  var r0 = (1 - refIdx) / (1 + refIdx)
+  r0 = r0 * r0
+  result = r0 + (1 - r0) * pow(1 - cosine, 5)
+
+proc scatter*(m: Dielectric, r_in: Ray, rec: HitRecord,
+              attenuation: var Color, scattered: var Ray): bool =
+  attenuation = color(1.0, 1.0, 1.0)
+  let refractionRatio = if rec.frontFace: (1.0 / m.ir) else: m.ir
+
+  let unitDirection = unitVector(r_in.dir)
+  let cosTheta = min(dot(-unitDirection, rec.normal), 1.0)
+  let sinTheta = sqrt(1.0 - cosTheta * cosTheta)
+
+  let cannotRefract = refraction_ratio * sinTheta > 1.0
+  var direction: Vec3
+
+  if cannotRefract or reflectance(cosTheta, refractionRatio) > rand(1.0):
+    direction = reflect(unitDirection, rec.normal)
+  else:
+    direction = refract(unitDirection, rec.normal, refractionRatio)
+
+  scattered = initRay(rec.p, direction)
+  result = true
+
+proc scatter*(m: Material, r_in: Ray, rec: HitRecord,
+              attenuation: var Color, scattered: var Ray): bool =
+  case m.kind
+  of mkLambertian: result = m.mLambertian.scatter(r_in, rec, attenuation, scattered)
+  of mkMetal: result = m.mMetal.scatter(r_in, rec, attenuation, scattered)
+  of mkDielectric: result = m.mDielectric.scatter(r_in, rec, attenuation, scattered)
